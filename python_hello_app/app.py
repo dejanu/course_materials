37ab9e50dcfadd29f5a15058afe8a6f9fc73f8ae
dejanualex <dejanualexandru@gmail.com>
2024-10-03 20:46:00 +0300

    connect to redis
    Signed-off-by: dejanualex <dejanualexandru@gmail.com>

--- python_hello_app/app.py
+++ python_hello_app/app.py
@@ -2,13 +2,18 @@
 # encoding: utf-8
 import json
 from flask import Flask
+from redis import Redis
 
+#redis on localhost
+redis = Redis(host='redis', port=6379)
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """return hello world text in bold"""
-    return '<br> <center><p><b>Hello World Python!</b>🐉</p></center>'
+    """Count and display no of page hits"""
+    redis.incr('hits') # inc the value of key by amount, if no key exists, the value will be initialized as
+    counter =  str(redis.get('hits'),'utf-8')
+    return f'<br> <center><p><b>Hello view no :{counter}</b>🐉</p></center>'
     
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000)
